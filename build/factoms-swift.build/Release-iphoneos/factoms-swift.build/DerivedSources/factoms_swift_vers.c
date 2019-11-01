2c2697f03c5ffd5d74b83adc01a6573534d7edc2
Sergey <sergey.bushnyak@sigrlami.eu>
2019-11-01 17:49:29 +0200

    cleanup

--- build/factoms-swift.build/Release-iphoneos/factoms-swift.build/DerivedSources/factoms_swift_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char factoms_swiftVersionString[];
- extern const double factoms_swiftVersionNumber;
-
- const unsigned char factoms_swiftVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:factoms_swift  PROJECT:factoms-swift-1" "\n";
- const double factoms_swiftVersionNumber __attribute__ ((used)) = (double)1.;
