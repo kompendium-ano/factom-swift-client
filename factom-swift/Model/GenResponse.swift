ef9a068c1890bd89e7760495ef4256fa746bc422
Sergey Bushnyak <sergey.bushnyak@sigrlami.eu>
2019-11-04 10:12:25 +0200

    update project structure and naming

---
+++ factom-swift/Model/GenResponse.swift
@@ -0,0 +1,29 @@
+//
+//  GenResponse.swift
+//  FactomApiTest
+//
+//  Created by Sergey Bushnyak on 12/11/19.
+//  Copyright © 2019-2020 Kompendium, LLC. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+public struct GenResponse:Mappable {
+    
+    public var jsonrpc:String = ""
+    public var id:Int?
+    public var result:JSONObject?
+    public var error:GenError?
+    
+    public init?(map: Map) {
+        
+    }
+    
+    public mutating func mapping(map: Map) {
+        jsonrpc     <- map["jsonrpc"]
+        id          <- map["id"]
+        result      <- map["result"]
+        error       <- map["error"]
+    }
+}
